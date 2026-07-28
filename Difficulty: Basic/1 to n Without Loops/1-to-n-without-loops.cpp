8e8c839770372cf758b230fe0045c865bb365a52
Devankit Yadav <devankityadav2020@gmail.com>
2026-07-28 18:35:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/1 to n Without Loops/1-to-n-without-loops.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+   
+  
+    void printTillN(int n) {
+        // code here
+        
+        if(n<1) return;
+        
+        printTillN(n-1);
+        
+        cout<<n<<" ";
+        
+    }
+};
